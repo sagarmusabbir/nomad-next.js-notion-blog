9dd1d4947ac29bdd217d8a66ad22808e456bc9a4
sagarmusabbir <sagarmusabbir@gmail.com>
2024-05-09 23:23:04 +0600

    first commit

---
+++ site.config.js
@@ -0,0 +1,74 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: "sagarsterminal",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "full stack next.js developer",
+    bio: "I develop everything using next.js.",
+    email: "sagarmusabbir@gmail.com",
+    linkedin: "musabbirsagar",
+    github: "sagarmusabbir",
+    instagram: "",
+  },
+  projects: [
+    {
+      name: `sagars-creation`,
+      href: "https://github.com/sagarmusabbir?tab=projects",
+    },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "sagars-log",
+    description: "welcome to sagars-log!",
+  },
+
+  // CONFIG configration (required)
+  link: "https://sagars-terminal.vercel.app",
+  since: 2022, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  naverSearchAdvisor: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: false,
+    config: {
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+}
+
+module.exports = { CONFIG }
